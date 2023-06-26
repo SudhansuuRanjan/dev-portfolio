48070207a6f33eaf1b15ec75198b83e446ff8dfc
SudhansuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2023-06-26 13:00:10 +0530

    🐛 (fix) : fixed Bugs

--- src/components/AnimatedCursor.tsx
+++ src/components/AnimatedCursor.tsx
@@ -1,24 +1,34 @@
 "use client"
-import AnimatedCursor from 'react-animated-cursor';
+import { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
+
+const DynamicAnimatedCursor = dynamic(() => import('react-animated-cursor'), { ssr: false });
 
 const AnimatedCursorComponent = ({ children }: { children: React.ReactNode }) => {
-        return (
-            <>
-                <AnimatedCursor
-                    innerSize={10}
-                    outerSize={42}
-                    color='168, 85, 247'
-                    innerScale={1.2}
-                    outerScale={1.5}
-                    outerStyle={{
-                        border: '2px solid gray',
-                        backgroundColor: 'transparent'
-                    }} />
-                {
-                    children
-                }
-            </>
-        )
-}
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return (
+    <>
+      {isMounted && (
+        <DynamicAnimatedCursor
+          innerSize={10}
+          outerSize={42}
+          color='168, 85, 247'
+          innerScale={1.2}
+          outerScale={1.5}
+          outerStyle={{
+            border: '2px solid gray',
+            backgroundColor: 'transparent'
+          }}
+        />
+      )}
+      {children}
+    </>
+  );
+};
 
 export default AnimatedCursorComponent;
