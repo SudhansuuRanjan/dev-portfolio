aa4b676bb2093e36e7297063d9fb63666b330c6c
SudhanshuuRanjan <sudhanshuranjan2k18@gmail.com>
2024-08-09 23:16:58 +0530

    fixed bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
                 c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments); };
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "YOUR_CLARITY_TRACKING_CODE");
+              })(window, document, "clarity", "script", "nkp1ne82u0");
             `,
           }}
         />
